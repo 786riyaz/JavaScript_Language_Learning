d6e8a18f4cecab33b36d9dcdaddb65e4f3056611
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-28 16:08:38 +0530

    Added Call Back Code

---
+++ 99 Topics to be Arranged/js/CallBack Code.js
@@ -0,0 +1,15 @@
+// Callback code
+var output = document.getElementById("output");
+
+function makeMessage(s1, s2, callback) {
+  var result1 = callback(s1, s2);
+  output.innerHTML += result1;
+}
+makeMessage("hello", "world", function (s1, s2) {
+    var result2 = s1 + s2 + "worked";
+    return result2;
+  }),
+  makeMessage("Java", "Script", function (s1, s2) {
+    var result2 = s1 + s2 + " is great" + s1;
+    return result2;
+  })
