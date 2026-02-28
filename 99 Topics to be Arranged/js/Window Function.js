9dd94b0031b32c3c1eeec14e3162edacce7880e7
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-02-28 16:08:19 +0530

    Added Some Pending Topics

---
+++ 99 Topics to be Arranged/js/Window Function.js
@@ -0,0 +1,29 @@
+// Window Object Methods 👨‍💻 
+
+var output = document.getElementById("wrapper");
+console.dir(window);
+
+for (var prop in window) {
+  document.write(prop + " " + window[prop] + "<br>");
+}
+output.innerHTML += window.innerHeight + " " + window.innerWidth;
+
+function goBack() {
+  window.history.go(-1);
+}
+// Print, prompt is a Builtin Function
+print();
+prompt();
+
+// Navigator object
+
+console.dir(Navigator)
+// To Know user Agent
+var output = document.getElementById("wrapper");
+output.innerHTML = navigator.userAgent;
+
+// DOM Traversing
+
+document.body.children
+document.body.children[3].innerHTML = "Google"
+document.body.children[4].children[1].children[1].name
