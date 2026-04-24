2e89e8154324ae93f1900508578f98160cdb0d92
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-04-24 18:02:12 +0530

    Added Loops Related Code and Details

--- 03 Control Flow Structure/02 Loops/03 For In/Example1.js
+++ 03 Control Flow Structure/02 Loops/03 For In/Example1.js
@@ -3,3 +3,9 @@ let array = [5, 10, 15, 20, 25, 30, 35, 40];
 for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
+
+console.log("====================================");
+
+for (a in array) {
+    console.log(array[a]);
+}
